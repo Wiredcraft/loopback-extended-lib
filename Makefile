dd787fbb1514c73f253c063b3938ba11278438bf
Makara Wang <makara15@gmail.com>
2016-02-23 14:15:49 +0800

    Added extended loopback and boot.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-ENV = NODE_ENV=test DEBUG=loopback:extended:*
+ENV = NODE_ENV=test DEBUG=loopback:extended:*,carcass:*
 MOCHA = ./node_modules/.bin/_mocha
-MOCHA_OPTS = -b --timeout 20000 --reporter spec
+MOCHA_OPTS = -b --timeout 3000 --reporter spec
 TESTS = test/*.test.js
 ISTANBUL = ./node_modules/.bin/istanbul
 COVERALLS = ./node_modules/.bin/coveralls
---
+++ test/lib.test.js
@@ -0,0 +1,63 @@
+var should = require('should');
+
+var lib = require('../lib');
+var DataSource = require('loopback-datasource-juggler').DataSource;
+
+describe('The lib', function () {
+
+  var app;
+
+  it('should be there', function () {
+    lib.should.have.type('object');
+    lib.should.have.property('extended').with.type('object');
+    lib.extended.should.have.property('loopback').with.type('function');
+    lib.extended.should.have.property('boot').with.type('function');
+  });
+
+  describe('The extended loopback', function () {
+
+    it('can create an app', function () {
+      app = lib.extended.loopback();
+      app.should.have.type('function');
+    });
+
+    it('can boot itself', function (done) {
+      app.should.have.property('boot').with.type('function');
+      app.boot(done);
+    });
+
+  });
+
+  describe('The extended boot', function () {
+
+    it('can boot an app', function (done) {
+      lib.extended.boot(app).then(function () {
+        app.should.have.property('dataSources').deepEqual({});
+        done();
+      }).catch(done);
+    });
+
+    it('can be used to replace data sources', function (done) {
+      lib.dataSources = lib.dataSources || {};
+      lib.dataSources.db = {
+        name: 'db',
+        connector: 'memory'
+      };
+      lib.extended.boot(app).then(function () {
+        app.dataSources.should.have.property('db').instanceof(DataSource);
+        done();
+      }).catch(done);
+    });
+
+    it('can be used to add connectors', function (done) {
+      lib.connectors = lib.connectors || {};
+      lib.connectors.Lorem = function () {};
+      lib.extended.boot(app).then(function () {
+        app.connectors.should.have.property('Lorem').with.type('function');
+        done();
+      }).catch(done);
+    });
+
+  });
+
+});
