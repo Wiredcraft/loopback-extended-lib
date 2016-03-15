f737d7acfbcd9df80cc7310a13fdca141d3bf4f0
Makara Wang <makara15@gmail.com>
2016-03-15 16:07:21 +0800

    Allow lib to come with actual models and mixins. Use the example as the test fixture.

--- test/10.extended.test.js
+++ test/10.extended.test.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var should = require('should');
 
 var lib = require('../lib');
@@ -6,6 +7,7 @@ var DataSource = require('loopback-datasource-juggler').DataSource;
 describe('The extended', function () {
 
   var app;
+  var exampleRoot = path.resolve(__dirname, '../example/server');
 
   describe('Loopback', function () {
 
@@ -14,7 +16,7 @@ describe('The extended', function () {
     });
 
     it('can create an app', function () {
-      app = lib.extended.loopback();
+      app = lib.extended.loopback(exampleRoot);
       app.should.have.type('function');
     });
 
@@ -32,36 +34,29 @@ describe('The extended', function () {
     });
 
     it('can boot an app', function (done) {
-      lib.extended.boot(app).then(function () {
-        app.should.have.property('dataSources').deepEqual({});
-        done();
-      }).catch(done);
-    });
-
-    it('can be used to replace data sources', function (done) {
-      lib.configs = lib.configs || {};
-      lib.configs.dataSources = lib.configs.dataSources || {};
-      lib.configs.dataSources.db = {
-        name: 'db',
-        connector: 'memory'
-      };
-      lib.extended.boot(app).then(function () {
+      lib.extended.boot(app, exampleRoot).then(function () {
+        app.should.have.property('dataSources').with.type('object');
         app.dataSources.should.have.property('db').instanceof(DataSource);
         done();
       }).catch(done);
     });
 
-    it('can be used to replace data sources and the data source config can be a function', function (done) {
+    it('can be used to add data sources', function (done) {
       lib.configs = lib.configs || {};
       lib.configs.dataSources = lib.configs.dataSources || {};
-      lib.configs.dataSources.db = function () {
-        return {
-          name: 'db',
-          connector: 'memory'
-        };
-      };
-      lib.extended.boot(app).then(function () {
-        app.dataSources.should.have.property('db').instanceof(DataSource);
+      lib.configs.dataSources.ipsum = require(path.resolve(exampleRoot, 'lib/configs/dataSources/ipsum'));
+      lib.extended.boot(app, exampleRoot).then(function () {
+        app.dataSources.should.have.property('ipsum').instanceof(DataSource);
+        done();
+      }).catch(done);
+    });
+
+    it('can be used to add data sources and the data source config can be a function', function (done) {
+      lib.configs = lib.configs || {};
+      lib.configs.dataSources = lib.configs.dataSources || {};
+      lib.configs.dataSources.lorem = require(path.resolve(exampleRoot, 'lib/configs/dataSources/lorem'));
+      lib.extended.boot(app, exampleRoot).then(function () {
+        app.dataSources.should.have.property('lorem').instanceof(DataSource);
         done();
       }).catch(done);
     });
@@ -69,8 +64,37 @@ describe('The extended', function () {
     it('can be used to add connectors', function (done) {
       lib.connectors = lib.connectors || {};
       lib.connectors.Lorem = function () {};
-      lib.extended.boot(app).then(function () {
+      lib.extended.boot(app, exampleRoot).then(function () {
         app.connectors.should.have.property('Lorem').with.type('function');
+        done();
+      }).catch(done);
+    });
+
+    it('can be used to add models', function (done) {
+      lib.configs = lib.configs || {};
+      lib.configs.models = lib.configs.models || {};
+      lib.configs.models.Lorem = {
+        dataSource: 'db',
+        public: false
+      };
+      lib.models = require(path.resolve(exampleRoot, 'lib/models'));
+      lib.extended.boot(app, exampleRoot).then(function () {
+        app.should.have.property('models').with.type('function');
+        app.models.should.have.property('Lorem').with.type('function');
+        done();
+      }).catch(done);
+    });
+
+    it('can be used to add mixins', function (done) {
+      lib.mixins = require(path.resolve(exampleRoot, 'lib/mixins'));
+      lib.extended.boot(app, exampleRoot).then(function () {
+        app.should.have.property('registry').with.type('object');
+        app.registry.should.have.property('modelBuilder').with.type('object');
+        app.registry.modelBuilder.should.have.property('mixins').with.type('object');
+        app.registry.modelBuilder.mixins.should.have.property('mixins').with.type('object');
+
+        // Name is changed from `lorem` to `Lorem` for some reason...
+        app.registry.modelBuilder.mixins.mixins.should.have.property('Lorem').with.type('function');
         done();
       }).catch(done);
     });
