563c5d68d4a9d443f81c6b501d88188b2fdd9c93
Makara Wang <makara15@gmail.com>
2016-04-15 12:56:16 +0800

    Updated modules. Switched to JSCS Grunt preset with minimal tweaks.

--- test/00.lib.test.js
+++ test/00.lib.test.js
@@ -2,18 +2,18 @@ var should = require('should');
 
 var lib = require('../lib');
 
-describe('The lib', function () {
+describe('The lib', function() {
 
-  it('should be there', function () {
+  it('should be there', function() {
     lib.should.have.type('object');
     lib.should.have.property('extended').with.type('object');
     lib.should.have.property('utils').with.type('object');
   });
 
-  it('can assert singleton', function () {
+  it('can assert singleton', function() {
     lib.should.have.property('assertSingleton').with.type('function');
     lib.assertSingleton(lib);
-    (function () {
+    (function() {
       lib.assertSingleton();
     }).should.throw();
   });
--- test/10.utils.test.js
+++ test/10.utils.test.js
@@ -3,82 +3,82 @@ var Joi = require('joi');
 
 var lib = require('../lib');
 
-describe('The utils', function () {
+describe('The utils', function() {
 
-  describe('The not empty object shortcut', function () {
+  describe('The not empty object shortcut', function() {
 
-    it('should be there', function () {
+    it('should be there', function() {
       lib.utils.should.have.property('notEmptyObject').with.type('function');
     });
 
-    it('can check a thing', function () {
+    it('can check a thing', function() {
       lib.utils.notEmptyObject({
         lorem: true
       }).should.equal(true);
     });
 
-    it('can check a thing', function () {
+    it('can check a thing', function() {
       lib.utils.notEmptyObject({}).should.equal(false);
     });
 
-    it('can check a thing', function () {
+    it('can check a thing', function() {
       lib.utils.notEmptyObject('lorem').should.equal(false);
     });
 
-    it('can check a thing', function () {
+    it('can check a thing', function() {
       lib.utils.notEmptyObject(null).should.equal(false);
     });
 
   });
 
-  describe('The not object or is empty shortcut', function () {
+  describe('The not object or is empty shortcut', function() {
 
-    it('should be there', function () {
+    it('should be there', function() {
       lib.utils.should.have.property('notObjectOrIsEmpty').with.type('function');
     });
 
-    it('can check a thing', function () {
+    it('can check a thing', function() {
       lib.utils.notObjectOrIsEmpty({
         lorem: true
       }).should.equal(false);
     });
 
-    it('can check a thing', function () {
+    it('can check a thing', function() {
       lib.utils.notObjectOrIsEmpty({}).should.equal(true);
     });
 
-    it('can check a thing', function () {
+    it('can check a thing', function() {
       lib.utils.notObjectOrIsEmpty('lorem').should.equal(true);
     });
 
-    it('can check a thing', function () {
+    it('can check a thing', function() {
       lib.utils.notObjectOrIsEmpty(null).should.equal(true);
     });
 
   });
 
-  describe('The reject shortcut', function () {
+  describe('The reject shortcut', function() {
 
-    it('should be there', function () {
+    it('should be there', function() {
       lib.utils.should.have.property('reject').with.type('function');
     });
 
-    it('can reject with an error code', function (done) {
-      lib.utils.reject(404).then(function () {
+    it('can reject with an error code', function(done) {
+      lib.utils.reject(404).then(function() {
         done(new Error('expected an error'));
-      }).catch(function (err) {
+      }).catch(function(err) {
         err.should.have.type('object');
         err.should.have.property('status', 404);
         done();
       }).catch(done);
     });
 
-    it('can reject with an error', function (done) {
+    it('can reject with an error', function(done) {
       var error = new Error('Lorem');
       error.status = 404;
-      lib.utils.reject(error).then(function () {
+      lib.utils.reject(error).then(function() {
         done(new Error('expected an error'));
-      }).catch(function (err) {
+      }).catch(function(err) {
         err.should.have.type('object');
         err.should.have.property('status', 404);
         err.should.have.property('message', 'Lorem');
@@ -86,13 +86,13 @@ describe('The utils', function () {
       }).catch(done);
     });
 
-    it('can reject with an error', function (done) {
+    it('can reject with an error', function(done) {
       var error = new Error('Lorem');
       error.status = 404;
       error.code = 123;
-      lib.utils.reject(error).then(function () {
+      lib.utils.reject(error).then(function() {
         done(new Error('expected an error'));
-      }).catch(function (err) {
+      }).catch(function(err) {
         err.should.have.type('object');
         err.should.have.property('status', 404);
         err.should.have.property('code', 123);
@@ -101,16 +101,16 @@ describe('The utils', function () {
       }).catch(done);
     });
 
-    it('can reject with an error', function (done) {
+    it('can reject with an error', function(done) {
       var error = new Error();
       error.status = 404;
       error.message = {
         code: 123,
         message: 'Lorem'
       };
-      lib.utils.reject(error).then(function () {
+      lib.utils.reject(error).then(function() {
         done(new Error('expected an error'));
-      }).catch(function (err) {
+      }).catch(function(err) {
         err.should.have.type('object');
         err.should.have.property('status', 404);
         err.should.have.property('code', 123);
@@ -119,7 +119,7 @@ describe('The utils', function () {
       }).catch(done);
     });
 
-    it('can reject with an error', function (done) {
+    it('can reject with an error', function(done) {
       var error = new Error();
       error.status = 404;
       error.message = {
@@ -128,9 +128,9 @@ describe('The utils', function () {
           message: 'Lorem'
         }
       };
-      lib.utils.reject(error).then(function () {
+      lib.utils.reject(error).then(function() {
         done(new Error('expected an error'));
-      }).catch(function (err) {
+      }).catch(function(err) {
         err.should.have.type('object');
         err.should.have.property('status', 404);
         err.should.have.property('code', 123);
@@ -139,7 +139,7 @@ describe('The utils', function () {
       }).catch(done);
     });
 
-    it('can reject with an error', function (done) {
+    it('can reject with an error', function(done) {
       var error = new Error();
       error.status = 404;
       error.message = {
@@ -148,9 +148,9 @@ describe('The utils', function () {
           message: 'Lorem'
         }]
       };
-      lib.utils.reject(error).then(function () {
+      lib.utils.reject(error).then(function() {
         done(new Error('expected an error'));
-      }).catch(function (err) {
+      }).catch(function(err) {
         err.should.have.type('object');
         err.should.have.property('status', 404);
         err.should.have.property('code', 123);
@@ -159,7 +159,7 @@ describe('The utils', function () {
       }).catch(done);
     });
 
-    it('can reject with an error', function (done) {
+    it('can reject with an error', function(done) {
       var error = new Error();
       error.status = 404;
       error.message = {
@@ -168,9 +168,9 @@ describe('The utils', function () {
           message: 'Lorem'
         }
       };
-      lib.utils.reject(error).then(function () {
+      lib.utils.reject(error).then(function() {
         done(new Error('expected an error'));
-      }).catch(function (err) {
+      }).catch(function(err) {
         err.should.have.type('object');
         err.should.have.property('status', 404);
         err.should.have.property('code', 123);
@@ -181,13 +181,13 @@ describe('The utils', function () {
 
   });
 
-  describe('The REST data source config builder', function () {
+  describe('The REST data source config builder', function() {
 
-    it('should be there', function () {
+    it('should be there', function() {
       lib.utils.should.have.property('restDataSourceConfig').with.type('function');
     });
 
-    it('can create a config', function () {
+    it('can create a config', function() {
       lib.utils.restDataSourceConfig({}).should.deepEqual({
         connector: 'rest',
         debug: false,
@@ -203,7 +203,7 @@ describe('The utils', function () {
       });
     });
 
-    it('can create a config', function () {
+    it('can create a config', function() {
       lib.utils.restDataSourceConfig({
         debug: true,
         options: {
@@ -229,7 +229,7 @@ describe('The utils', function () {
       });
     });
 
-    it('can create a config', function () {
+    it('can create a config', function() {
       lib.utils.restDataSourceConfig({}, [{
         template: {
           method: 'GET',
@@ -266,7 +266,7 @@ describe('The utils', function () {
       });
     });
 
-    it('can create a config', function () {
+    it('can create a config', function() {
       lib.utils.restDataSourceConfig({
         debug: true,
         options: {
@@ -316,89 +316,89 @@ describe('The utils', function () {
 
   });
 
-  describe('The value obtainer builder', function () {
+  describe('The value obtainer builder', function() {
 
     var schema = Joi.object({
       lorem: Joi.string().trim()
     });
 
-    it('should be there', function () {
+    it('should be there', function() {
       lib.utils.should.have.property('valueObtainer').with.type('function');
     });
 
-    it('can build a function', function () {
+    it('can build a function', function() {
       lib.utils.valueObtainer('xxx', schema).should.have.type('function');
     });
 
-    it('can build a function', function () {
+    it('can build a function', function() {
       lib.utils.valueObtainer('xxx', schema, 'lorem').should.have.type('function');
     });
 
-    it('can build a function', function () {
+    it('can build a function', function() {
       lib.utils.valueObtainer(schema).should.have.type('function');
     });
 
-    it('can build a function', function () {
+    it('can build a function', function() {
       lib.utils.valueObtainer(schema, 'lorem').should.have.type('function');
     });
 
-    it('can throw', function () {
-      (function () {
+    it('can throw', function() {
+      (function() {
         lib.utils.valueObtainer();
       }).should.throw();
     });
 
-    it('can throw', function () {
-      (function () {
+    it('can throw', function() {
+      (function() {
         lib.utils.valueObtainer({}, 'lorem');
       }).should.throw();
     });
 
-    it('can throw', function () {
-      (function () {
+    it('can throw', function() {
+      (function() {
         lib.utils.valueObtainer(Joi.string(), 'lorem');
       }).should.throw();
     });
 
-    it('can throw', function () {
-      (function () {
+    it('can throw', function() {
+      (function() {
         lib.utils.valueObtainer('lorem', {}, 'lorem');
       }).should.throw();
     });
 
-    describe('An obtainer', function () {
+    describe('An obtainer', function() {
 
       var ensureLorem;
 
-      it('can be created', function () {
+      it('can be created', function() {
         ensureLorem = lib.utils.valueObtainer('ensureLorem', schema, 'lorem');
         ensureLorem.should.have.type('function');
       });
 
-      it('can validate and get value', function () {
+      it('can validate and get value', function() {
         ensureLorem({
           lorem: 'lorem ipsum'
         }).should.equal('lorem ipsum');
       });
 
-      it('can throw', function () {
-        (function () {
+      it('can throw', function() {
+        (function() {
           ensureLorem({
             lorem: 123
           });
         }).should.throw();
       });
 
-      it('can throw', function () {
-        (function () {
+      it('can throw', function() {
+        (function() {
           ensureLorem({
             ipsum: 'lorem ipsum'
           });
         }).should.throw();
       });
 
-      it('can throw', function () {
-        (function () {
+      it('can throw', function() {
+        (function() {
           ensureLorem('lorem');
         }).should.throw();
       });
